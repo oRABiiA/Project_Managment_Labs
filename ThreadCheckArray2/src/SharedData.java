7877ca111809b7ba1f14f8488158973fdf8f4b50
rabealahham2 <rabealahham2@gmail.com>
2024-01-21 12:50:31 +0200

    sadasd

---
+++ ThreadCheckArray2/src/SharedData.java
@@ -0,0 +1,94 @@
+public class SharedData {
+
+	/**
+	 * A class which holds data for the threads
+	 * 
+	 * @author Rabea Lahham
+	 * @author Majd Salameh
+	 *
+	 */
+
+	private int[] array;
+	private boolean[] winArray;
+	private boolean flag;
+	private final int b;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param array           a list of integers
+	 * @param b               an integer value
+	 * 
+	 */
+	public SharedData(int[] array, int b) {
+
+		this.array = array;
+		this.b = b;
+	}
+
+	/**
+	 * getter for winArray
+	 * 
+	 * @return                returns winArray in the object
+	 * 
+	 */
+
+	public boolean[] getWinArray() {
+		return winArray;
+	}
+
+	/**
+	 * setter for winArray
+	 * 
+	 * @param winArray         an array of boolean values
+	 * 
+	 */
+
+	public void setWinArray(boolean[] winArray) {
+		this.winArray = winArray;
+	}
+
+	/**
+	 * getter for array variable
+	 * 
+	 * @return               returns array in the object
+	 * 
+	 */
+
+	public int[] getArray() {
+		return array;
+	}
+
+	/**
+	 * getter for B variable
+	 * 
+	 * @return                returns B variable in the object
+	 * 
+	 */
+
+	public int getB() {
+		return b;
+	}
+
+	/**
+	 * getter for flag variable
+	 * 
+	 * @return                 returns the flag in the object
+	 * 
+	 */
+
+	public boolean getFlag() {
+		return flag;
+	}
+
+	/**
+	 * setter for flag
+	 * 
+	 * @param flag               holds a boolean value
+	 * 
+	 */
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+
+}
